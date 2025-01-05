ecd32e805d868f9e36018f45e6e75c893511e1f6
Kenneth Davidson <xylene2016@protonmail.com>
2025-01-05 03:08:52 -0500

    Update build-nginx.sh

--- ub2204/build-nginx.sh
+++ ub2204/build-nginx.sh
@@ -796,7 +796,8 @@ chmod 0644 etc/sysconfig/nginx
     chmod 0644 etc/nginx/conf.d/default.conf
     wget -c -t 9 -T 9 'https://raw.githubusercontent.com/icebluey/build-nginx/refs/heads/master/conf/h2.conf' -O etc/nginx/conf.d/h2.conf.example
     wget -c -t 9 -T 9 'https://raw.githubusercontent.com/icebluey/build-nginx/refs/heads/master/conf/h3.conf' -O etc/nginx/conf.d/h3.conf.example
-    chmod 0644 etc/nginx/conf.d/h*
+    wget -c -t 9 -T 9 'https://raw.githubusercontent.com/icebluey/build-nginx/refs/heads/master/conf/opt.conf' -O etc/nginx/conf.d/opt.conf.example
+    chmod 0644 etc/nginx/conf.d/*conf*
     sleep 2
     tar -Jcvf /tmp/nginx-"${_nginx_ver}"-1_ub2204_amd64.tar.xz *
     echo
