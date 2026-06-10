56901c8197cf21134d3fdbfe7fa23698c9570983
Kenneth Davidson <xylene2016@protonmail.com>
2026-06-10 17:15:40 -0400

    Update build-nginx-aws-lc.sh

--- al8/build-nginx-aws-lc.sh
+++ al8/build-nginx-aws-lc.sh
@@ -420,9 +420,9 @@ _build_nginx() {
     #wget -c -t 9 -T 9 'https://raw.githubusercontent.com/icebluey/build-nginx/refs/heads/master/nginx-patches/aws-lc-nginx-1.29.1.patch' -O /tmp/aws-lc-nginx.patch
     #patch -N -p 1 -i /tmp/aws-lc-nginx.patch
     #rm -f /tmp/aws-lc-nginx.patch
-    _vmajor=2
-    _vminor=9
-    _vpatch=14
+    _vmajor=3
+    _vminor=1
+    _vpatch=11
     _longver=$(printf "%1d%03d%03d" ${_vmajor} ${_vminor} ${_vpatch})
     _fullver="$(echo \"${_vmajor}\.${_vminor}\.${_vpatch}\")"
     sed "s@#define nginx_version.*@#define nginx_version      ${_longver}@g" -i src/core/nginx.h
